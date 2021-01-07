5026e844f896de34657a7ea74153b53ca669ef54
Pramit Dave <p.dave@axelor.com>
2021-01-07 17:05:06 +0530

    Border color support for Excel to Html

--- axelor-base/src/main/java/com/axelor/apps/base/service/excelreport/html/XSSFHtmlHelper.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/excelreport/html/XSSFHtmlHelper.java
@@ -17,13 +17,19 @@
  */
 package com.axelor.apps.base.service.excelreport.html;
 
+import com.axelor.common.ObjectUtils;
+import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Formatter;
+import java.util.List;
 import java.util.Map;
 import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.extensions.XSSFCellBorder.BorderSide;
 
 public class XSSFHtmlHelper implements HtmlHelper {
   private final XSSFWorkbook wb;
@@ -36,8 +42,23 @@ public class XSSFHtmlHelper implements HtmlHelper {
 
   public void colorStyles(CellStyle style, Formatter out) {
     XSSFCellStyle cs = (XSSFCellStyle) style;
+    List<XSSFColor> borderColorList = new ArrayList<>();
+
     styleColor(out, "background-color", cs.getFillForegroundXSSFColor());
     styleColor(out, "color", cs.getFont().getXSSFColor());
+
+    borderColorList.add(
+        cs.getBorderTop().equals(BorderStyle.NONE) ? null : cs.getBorderColor(BorderSide.TOP));
+    borderColorList.add(
+        cs.getBorderRight().equals(BorderStyle.NONE) ? null : cs.getBorderColor(BorderSide.RIGHT));
+    borderColorList.add(
+        cs.getBorderBottom().equals(BorderStyle.NONE)
+            ? null
+            : cs.getBorderColor(BorderSide.BOTTOM));
+    borderColorList.add(
+        cs.getBorderLeft().equals(BorderStyle.NONE) ? null : cs.getBorderColor(BorderSide.LEFT));
+
+    styleBorderColor(out, "border-color", borderColorList);
   }
 
   private void styleColor(Formatter out, String attr, XSSFColor color) {
@@ -58,5 +79,21 @@ public class XSSFHtmlHelper implements HtmlHelper {
     }
     out.format(
         "  %s: rgba(0x%02x, 0x%02x, 0x%02x, 0x%02x);%n", attr, argb[3], argb[0], argb[1], argb[2]);
+  }
+
+  private void styleBorderColor(Formatter out, String attr, List<XSSFColor> colorList) {
+    if (colorList.isEmpty()) {
+      return;
+    }
+
+    XSSFColor black = new XSSFColor(Color.black, wb.getStylesSource().getIndexedColors());
+    StringBuilder borderColor = new StringBuilder();
+
+    for (XSSFColor color : colorList) {
+      byte[] rgb = ObjectUtils.isEmpty(color) ? black.getRGB() : color.getRGB();
+      borderColor.append(String.format(" #%02x%02x%02x", rgb[0], rgb[1], rgb[2]));
+    }
+
+    out.format("  %1$s: %2$s;%n", attr, borderColor);
   }
 }
